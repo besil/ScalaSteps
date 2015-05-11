39efa59fef15324c409b068c3ff5e62b41bba8d1
besil <s.bernardinello@reply.it>
2015-05-11 16:38:24 +0200

    Reverse added

--- src/main/scala/MyList.scala
+++ src/main/scala/MyList.scala
@@ -2,12 +2,32 @@
  * Created by besil on 09/05/15.
  */
 object MyList {
+  def reverse[T](l: List[T]): List[T] = l match {
+    case Nil => Nil
+    case h :: tail => reverse(tail) ::: List(h)
+  }
+
+  def length[T](l: List[T]) = {
+    def count(n: Int, l: List[T]): Int = l match {
+      case Nil => n
+      case h :: tail => count(n+1, tail)
+    }
+    count(0, l)
+  }
+
+  def nth[T](k: Int, l: List[T]): T = {
+    def next(k: Int, l: List[T]): T = k match {
+      case 0 => l.head
+      case _ => next(k-1, l.tail)
+    }
+    next(k, l)
+  }
+
   def penultimate[T](l: List[T]): T = l match {
     case Nil => throw new NoSuchElementException
     case pe :: ul :: Nil => pe
     case h :: tail => penultimate(tail)
   }
-
 
   def last[T](l: List[T]): T = l match {
     case Nil => throw new NoSuchElementException
--- src/test/scala/MyListTest.scala
+++ src/test/scala/MyListTest.scala
@@ -4,16 +4,16 @@ import org.scalatest.FunSuite
  * Created by besil on 09/05/15.
  */
 class MyListTest extends FunSuite {
-  test("Last") {
-    val l = 1 to 5 toList
+  val l = 1 to 5 toList
 
-    assert( MyList.last(l) == 5 )
-  }
+  test("Reverse") { assert( MyList.reverse(List(1, 1, 2, 3, 5, 8)) == List(8, 5, 3, 2, 1, 1) ) }
 
-  test("Penultimate") {
-    val l = 1 to 5 toList
+  test("Length") { assert(MyList.length(List(1, 1, 2, 3, 5, 8)) == 6)}
 
-    assert( MyList.penultimate(l) == 4 )
-  }
+  test("Nth") { assert(MyList.nth(2, List(1, 1, 2, 3, 5, 8)) == 2) }
+
+  test("Last") { assert( MyList.last(l) == 5 ) }
+
+  test("Penultimate") { assert( MyList.penultimate(l) == 4 ) }
 
 }
